4093a5ec36f44484e014cf265a344bd81a4b879d
austin <a@spooky.im>
2017-03-06 17:29:27 -0500

    auto-commit from makefile

--- spriteDisplay.asm
+++ spriteDisplay.asm
@@ -42,10 +42,10 @@ Start:
 	;stz $0002	;starting at tile 0 of tileset
 	;lda #%01110000	;horizontal flip, top priority
 	;sta $0003
-	stz $0000
-	stz $0001
-	stz $0002
-	stz $0003
+	;stz $0000
+	;stz $0001
+	;stz $0002
+	;stz $0003
 	;lda #($80-16)
 	;sta $0004
 	;lda #(224/2-16)
