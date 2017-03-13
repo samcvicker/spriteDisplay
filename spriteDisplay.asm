7a8d88c51089fc1bd301f5676919b41d37363d63
austin <a@spooky.im>
2017-03-12 21:49:01 -0400

    auto-commit from makefile

--- spriteDisplay.asm
+++ spriteDisplay.asm
@@ -92,7 +92,7 @@ _offscreen:
 	cpx #$0200	;200 is the size of the first OAM table
 	bne _offscreen
 	ldx #$0000
-	lda #$0000	;CHANGE THIS TO #$5555 (01010101)
+	lda #$5555	;CHANGE THIS TO #$5555 (01010101)
 _clr:
 	sta $0200, X	;increases 2 times bc 2 bits per sprite
 	inx	;bit1 - enable or disable the x coordinate's 9th bit
